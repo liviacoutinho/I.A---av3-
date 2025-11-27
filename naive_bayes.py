a173312b07582579e4eeeb888f893189752ae963
Lívia Coutinho <95651497+liviacoutinho@users.noreply.github.com>
2025-11-27 18:34:00 -0300

    Add files via upload

--- naive_bayes.py
+++ naive_bayes.py
@@ -135,7 +135,7 @@ def plot_pie_graph(y_test, y_pred):
         labels=["Acertos", "Erros"],
         autopct="%1.1f%%",
         colors=["#be76d0", "#c4ecff"],
-        textprops={"color": "white"}  # labels brancas
+        textprops={"color": "white"}  
     )
 
     texto = f"Acurácia: {correct / (correct + incorrect):.4f}   |   F1 Score: {f1:.4f}"
